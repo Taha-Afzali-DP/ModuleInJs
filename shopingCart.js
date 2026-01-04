53731a6e28d7039ad36fafb99b771e307e048fa0
Taha_Afzali <elfo.dp8787@gmail.com>
2026-01-04 16:29:15 +0330

    part 5

--- shopingCart.js
+++ shopingCart.js
@@ -2,3 +2,7 @@
 console.log("Exporting module");
 const shopingCost = 10;
 const cart = [];
+const addToCart = function (products, quantity) {
+  cart.push({ products, quantity });
+  console.log(`${quantity} ${products} added to cart`);
+};
