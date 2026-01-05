1ec983041a629e98c412f3a3aa990b72dfcac425
Taha_Afzali <elfo.dp8787@gmail.com>
2026-01-05 22:06:45 +0330

    part 5

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //TODO Importing module
-import { addToCart, totalPrice as price, tq } from "./shopingCart.js";
-console.log("Importing module");
-addToCart("watch", 10);
-console.log(price, tq);
+// import { addToCart, totalPrice as price, tq } from "./shopingCart.js";
+// console.log("Importing module");
+// addToCart("watch", 10);
+// console.log(price, tq);
+import * as shopingCart from "./shopingCart.js";
