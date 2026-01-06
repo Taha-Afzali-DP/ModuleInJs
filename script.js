f85587e365b664c3bea5fbf55a351e608c67ad01
Taha_Afzali <elfo.dp8787@gmail.com>
2026-01-06 13:13:14 +0330

    part 6

--- script.js
+++ script.js
@@ -15,3 +15,9 @@ testDefaultExporte("watch", 100);
 // const data = await res.json();
 // console.log(data);
 // console.log("finish feching");
+
+const getLastPost = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  console.log(data);
+};
