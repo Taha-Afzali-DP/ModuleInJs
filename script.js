1c368bb2c3d05bfa75380ab1e742a5a0590aa326
Taha_Afzali <elfo.dp8787@gmail.com>
2026-01-07 21:50:51 +0330

    part 10

--- script.js
+++ script.js
@@ -56,3 +56,14 @@ shopingCart.addToCart("Coin", 30);
 shopingCart.addToCart("kif", 26);
 console.log(shopingCart);
 import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+
+const state = {
+  cart: [
+    { product: "bread", quantity: 5 },
+    { product: "pizza", quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+console.log(stateClone);
